2b225dbafd7bf69fe2b5f762a74911a6b267fbab
Devika1304 <61641024+Devika1304@users.noreply.github.com>
2023-10-10 15:26:54 +0530

    Delete EmployeeService directory

--- EmployeeService/src/main/java/com/employee/service/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.employee.service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.employee.service.entity.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Long> {
-
-}
